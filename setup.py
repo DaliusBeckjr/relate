a04476b8309061ee5fd488233765643507ceee79
Dalius Beck <daliusbeckxiv@gmail.com>
2024-10-06 22:45:06 -0700

    fixed discovery issue with uv run

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="relate",
+    version="0.1",
+    packages=find_packages(include=['backend', 'frontend']),
+    # other configurations
+)
