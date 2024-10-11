5f4a52d545e5ca53bba3136e253bf88db298bf48
Dalius Beck <daliusbeckxiv@gmail.com>
2024-10-10 22:11:07 -0700

    added a logout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import { NavLink } from "react-router-dom"
+import { useLogout } from "../hooks/useLogout";
 
 
 
 const Navbar = () => {
+    const { logout } = useLogout();
+
+    const handleClick = () => {
+        logout();
+
+    }
+
     return (
         // going to make this navbar responsive but a bit more complicated and different to give it a nice look and feel. want something alluring for a blog app. 
         <nav className="flex justify-between items-center  bg-slate-200 rounded-xl py-4 px-6 glass dark:glass-dark">
@@ -10,8 +18,9 @@ const Navbar = () => {
                 <div className="">
                 <NavLink to='/' className="hover:underline hover: decoration-2 hover:underline-offset-8 hover:decoration-indigo-500 text-slate-800 font-bold text-base  mx-5"> Home</NavLink> 
                 <NavLink to='/create' className="btn btn-primary">Create Post</NavLink>
-                <NavLink to='/login'>Login</NavLink>
-                <NavLink to='/signup'>Signup</NavLink>
+                <NavLink to='/login'>Login</NavLink> |
+                <NavLink to='/signup'>Signup</NavLink> | 
+                <button onClick={handleClick}>Logout</button>
                 </div>
         </nav>
     )
